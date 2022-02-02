2f0f5070a00fde7c95fe87fc205a7887dd4344d9
Hp <ad390443@gmail.com>
2022-02-02 15:29:18 +0500

    ..

--- app/src/main/java/uz/datamond/barbershop/ui/barbers/BarbersFragment.kt
+++ app/src/main/java/uz/datamond/barbershop/ui/barbers/BarbersFragment.kt
@@ -5,18 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import uz.datamond.barbershop.R
 import uz.datamond.barbershop.databinding.FragmentBarbersBinding
 
 class BarbersFragment : Fragment() {
 
     private lateinit var binding: FragmentBarbersBinding
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
 
     ): View? {
         binding = FragmentBarbersBinding.inflate(layoutInflater, container, false)
+
+        binding.tvAaa.setOnClickListener {
+            findNavController().navigate(R.id.navigation_barber_info)
+        }
+
         return binding.root
     }
 
